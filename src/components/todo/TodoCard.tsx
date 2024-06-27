e9db20046b077c75ba25a266e24c5b846f7aac57
Rabby <mdrabbybd4747@gmail.com>
2024-06-27 22:59:57 +0600

    create a reducers and to add a todo task

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -2,14 +2,19 @@ import Trash from "@/assets/icons/Trash"
 import { Button } from "../ui/button"
 import PenSquare from "@/assets/icons/PenSquare"
 
-const TodoCard = () => {
+type TTodoCardProps = {
+	title: string
+	description: string
+}
+
+const TodoCard = ({ title, description }: TTodoCardProps) => {
 	return (
 		<div>
 			<div className="bg-white rounded-md flex justify-between items-center p-3 border">
 				<input type="checkbox" name="" id="" />
-				<p className="font-semibold">Todo Title</p>
-				<p>Time</p>
-				<p>Description</p>
+				<p className="font-semibold">{title}</p>
+				{/* <p>Time</p> */}
+				<p>{description}</p>
 				<div className="space-x-5">
 					<Button className="bg-red-500">
 						<Trash />
