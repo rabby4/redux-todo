382b706aeb8c02c5a3942802113e5bb823ba017d
Rabby <mdrabbybd4747@gmail.com>
2024-06-29 20:35:08 +0600

    createApi with redux and connect with backend

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -2,9 +2,16 @@ import { useAppSelector } from "@/redux/hook"
 import AddTodoModal from "./AddTodoModal"
 import TodoCard from "./TodoCard"
 import TodoFilter from "./TodoFilter"
+import { useGetTodosQuery } from "@/redux/api/api"
 
 const TodoContainer = () => {
-	const { todos } = useAppSelector((state) => state.todos)
+	// From local state
+	// const { todos } = useAppSelector((state) => state.todos)
+
+	// From server
+
+	const { data: todos, isLoading, isError } = useGetTodosQuery(undefined)
+
 	return (
 		<div>
 			<div className="flex justify-between mb-5">
@@ -16,7 +23,7 @@ const TodoContainer = () => {
 					<p>There is no task pending</p>
 				</div> */}
 				<div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
-					{todos?.map((item) => (
+					{todos?.data?.map((item) => (
 						<TodoCard {...item} />
 					))}
 				</div>
---
+++ src/redux/api/api.ts
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+export const baseApi = createApi({
+	reducerPath: "baseApi",
+	baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+	endpoints: (builder) => ({
+		postTodos: builder.query({
+			query: () => ({
+				url: "/tasks",
+				method: "POST",
+			}),
+		}),
+		getTodos: builder.query({
+			query: () => ({
+				url: "/tasks",
+				method: "GET",
+			}),
+		}),
+	}),
+})
+
+export const { useGetTodosQuery } = baseApi
